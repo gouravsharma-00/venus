3321ff07296ef371d8aa3866d3e1fe56a33c63a4
gouravsharma-00 <gourav.sharma.glamat2932@gmail.com>
2025-06-10 21:11:04 +0000

    info

--- app/page.tsx
+++ app/page.tsx
@@ -57,9 +57,33 @@ const editEntry = async (e) => {
   setFrom(e.from)
 }
 
+const handleInfo = () => {
+  alert(`The Product Manager Accelerator Program is designed to support PM professionals through every stage of their careers. From students looking for entry-level jobs to Directors looking to take on a leadership role, our program has helped over hundreds of students fulfill their career aspirations.
+
+Our Product Manager Accelerator community are ambitious and committed. Through our program they have learnt, honed and developed new PM and leadership skills, giving them a strong foundation for their future endeavors.
+
+Here are the examples of services we offer. Check out our website (link under my profile) to learn more about our services.
+
+🚀 PMA Pro
+End-to-end product manager job hunting program that helps you master FAANG-level Product Management skills, conduct unlimited mock interviews, and gain job referrals through our largest alumni network. 25% of our offers came from tier 1 companies and get paid as high as $800K/year.
+
+🚀 AI PM Bootcamp
+Gain hands-on AI Product Management skills by building a real-life AI product with a team of AI Engineers, data scientists, and designers. We will also help you launch your product with real user engagement using our 100,000+ PM community and social media channels.
+
+🚀 PMA Power Skills
+Designed for existing product managers to sharpen their product management skills, leadership skills, and executive presentation skills
+
+🚀 PMA Leader
+We help you accelerate your product management career, get promoted to Director and product executive levels, and win in the board room.
+
+🚀 1:1 Resume Review
+We help you rewrite your killer product manager resume to stand out from the crowd, with an interview guarantee.  Get started by using our FREE killer PM resume template used by over 14,000 product managers. https://www.drnancyli.com/pmresume
+
+🚀 We also published over 500+ free training and courses. Please go to my YouTube channel https://www.youtube.com/c/drnancyli and Instagram @drnancyli to start learning for free today.`)
+}
   return (
     <div>
-      <h1>Weather CRUD App</h1>
+      <h1>Gourav Sharma - Weather CRUD App, About <button onClick={handleInfo}>PM Accelerator</button> </h1>
       <div>
         <select value={method} onChange={e=>setMethod(e.target.value)}>
           <option value="zip">Zip</option>
